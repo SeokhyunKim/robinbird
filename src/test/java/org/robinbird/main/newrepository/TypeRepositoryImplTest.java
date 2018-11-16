53456f6a3302cb2240f5dc23e136828510c45221
seokhyun <seokhyun.kim.78@gmail.com>
2018-11-16 05:52:10 -1000

    temp

--- src/test/java/org/robinbird/main/newrepository/TypeRepositoryImplTest.java
+++ src/test/java/org/robinbird/main/newrepository/TypeRepositoryImplTest.java
@@ -1,4 +1,34 @@
 package org.robinbird.main.newrepository;
 
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.robinbird.main.newmodel.Type;
+import org.robinbird.main.newmodel.TypeCategory;
+import org.robinbird.main.newrepository.dao.TypeDao;
+import org.robinbird.main.newrepository.dao.TypeDaoFactory;
+
 public class TypeRepositoryImplTest {
+
+    private TypeDao dao = TypeDaoFactory.createDao("mem:");
+
+    private TypeRepository repository = new TypeRepositoryImpl(dao);
+
+    @Test
+    public void test_registerType_getType_deleteType() {
+        final Type t = repository.registerType(TypeCategory.CLASS, "test");
+        Assert.assertThat(t.getCategory(), is(TypeCategory.CLASS));
+        Assert.assertThat(t.getName(), is("test"));
+
+        final Type loaded1 = repository.getType(t.getId()).get();
+        final Type loaded2 = repository.getType("test").get();
+        Assert.assertThat(t, is(loaded1));
+        Assert.assertThat(t, is(loaded2));
+
+        repository.deleteType(t.getId());
+        final Type loaded3 = repository.getType(t.getId()).get();
+        Assert.assertNull(loaded3);
+    }
+
 }
--- src/test/java/org/robinbird/main/newrepository/dao/TypeDaoImplTest.java
+++ src/test/java/org/robinbird/main/newrepository/dao/TypeDaoImplTest.java
@@ -20,8 +20,7 @@ import org.robinbird.main.newrepository.dao.entity.TypeEntity;
 
 public class TypeDaoImplTest {
 
-    private TypeDaoImpl daoImpl =
-            (TypeDaoImpl)TypeDaoFactory.createDao("mem:");
+    private TypeDao dao = TypeDaoFactory.createDao("mem:");
 
     @Test
     public void test_save_load_remove_TypeEntity() {
@@ -29,14 +28,14 @@ public class TypeDaoImplTest {
         entity.setCategory(TypeCategory.CLASS.name());
         entity.setName("test");
 
-        final TypeEntity saved = daoImpl.saveTypeEntity(entity);
-        final Optional<TypeEntity> loadedOpt = daoImpl.loadTypeEntity(saved.getId());
+        final TypeEntity saved = dao.saveTypeEntity(entity);
+        final Optional<TypeEntity> loadedOpt = dao.loadTypeEntity(saved.getId());
 
         Assert.assertTrue(loadedOpt.isPresent());
         Assert.assertThat(saved, is(loadedOpt.get()));
 
-        daoImpl.removeTypeEntity(loadedOpt.get());
-        final Optional<TypeEntity> loadedAgain = daoImpl.loadTypeEntity(saved.getId());
+        dao.removeTypeEntity(loadedOpt.get());
+        final Optional<TypeEntity> loadedAgain = dao.loadTypeEntity(saved.getId());
 
         Assert.assertFalse(loadedAgain.isPresent());
     }
@@ -49,15 +48,15 @@ public class TypeDaoImplTest {
         final TypeEntity entity1 = new TypeEntity();
         entity1.setCategory(TypeCategory.CLASS.name());
         entity1.setName(name1);
-        daoImpl.saveTypeEntity(entity1);
+        dao.saveTypeEntity(entity1);
 
         final TypeEntity entity2 = new TypeEntity();
         entity2.setCategory(TypeCategory.CLASS.name());
         entity2.setName(name2);
-        daoImpl.saveTypeEntity(entity2);
+        dao.saveTypeEntity(entity2);
 
-        final Optional<TypeEntity> loadedOpt1 = daoImpl.loadTypeEntity(name1);
-        final Optional<TypeEntity> loadedOpt2 = daoImpl.loadTypeEntity(name2);
+        final Optional<TypeEntity> loadedOpt1 = dao.loadTypeEntity(name1);
+        final Optional<TypeEntity> loadedOpt2 = dao.loadTypeEntity(name2);
 
         Assert.assertThat(loadedOpt1.get(), is(entity1));
         Assert.assertThat(loadedOpt2.get(), is(entity2));
@@ -68,14 +67,14 @@ public class TypeDaoImplTest {
         final TypeEntity entity = new TypeEntity();
         entity.setCategory(TypeCategory.CLASS.name());
         entity.setName("test1");
-        daoImpl.saveTypeEntity(entity);
+        dao.saveTypeEntity(entity);
 
-        final Optional<TypeEntity> loaded = daoImpl.loadTypeEntity(entity.getId());
+        final Optional<TypeEntity> loaded = dao.loadTypeEntity(entity.getId());
         Assert.assertThat(loaded.get(), is(entity));
 
         entity.setCategory("updated");
-        daoImpl.updateTypeEntity(entity);
-        final Optional<TypeEntity> updated = daoImpl.loadTypeEntity(entity.getId());
+        dao.updateTypeEntity(entity);
+        final Optional<TypeEntity> updated = dao.loadTypeEntity(entity.getId());
         Assert.assertThat(updated.get(), is(entity));
     }
 
@@ -87,23 +86,23 @@ public class TypeDaoImplTest {
         long typeId = generator.nextLong(1L, 1000L);
         entity1.setTypeId(typeId);
         entity1.setCompositionTypeId(1L);
-        daoImpl.saveCompositionTypeEntity(entity1);
+        dao.saveCompositionTypeEntity(entity1);
 
         final CompositionTypeEntity entity2 = new CompositionTypeEntity();
         entity2.setTypeId(typeId);
         entity2.setCompositionTypeId(2L);
-        daoImpl.saveCompositionTypeEntity(entity2);
+        dao.saveCompositionTypeEntity(entity2);
 
 
-        final List<CompositionTypeEntity> compTypes = daoImpl.loadCompositionTypeEntities(typeId);
+        final List<CompositionTypeEntity> compTypes = dao.loadCompositionTypeEntities(typeId);
 
         Assert.assertThat(compTypes.size(), is(2));
         Set<Long> compIds = compTypes.stream().map(CompositionTypeEntity::getCompositionTypeId).collect(Collectors.toSet());
         Assert.assertTrue(compIds.contains(1L));
         Assert.assertTrue(compIds.contains(2L));
 
-        daoImpl.removeCompositionTypeEntities(typeId);
-        final List<CompositionTypeEntity> removedTypes = daoImpl.loadCompositionTypeEntities(typeId);
+        dao.removeCompositionTypeEntities(typeId);
+        final List<CompositionTypeEntity> removedTypes = dao.loadCompositionTypeEntities(typeId);
         Assert.assertTrue(removedTypes.isEmpty());
     }
 
@@ -116,23 +115,43 @@ public class TypeDaoImplTest {
         entity1.setTypeId(1L);
         entity1.setAccessModifier(AccessModifier.PUBLIC.name());
         entity1.setName("entity1");
-        daoImpl.saveInstanceEntity(entity1);
+        dao.saveInstanceEntity(entity1);
 
         final InstanceEntity entity2 = new InstanceEntity();
         entity2.setParentTypeId(parentTypeId);
         entity2.setTypeId(2L);
         entity2.setAccessModifier(AccessModifier.PUBLIC.name());
         entity2.setName("entity2");
-        daoImpl.saveInstanceEntity(entity2);
+        dao.saveInstanceEntity(entity2);
 
-        final List<InstanceEntity> loadedEntities = daoImpl.loadInstanceEntities(parentTypeId);
+        final List<InstanceEntity> loadedEntities = dao.loadInstanceEntities(parentTypeId);
         Assert.assertThat(loadedEntities.size(), is(2));
         final Set<Long> typeIds = loadedEntities.stream().map(InstanceEntity::getTypeId).collect(Collectors.toSet());
         Assert.assertTrue(typeIds.contains(1L));
         Assert.assertTrue(typeIds.contains(2L));
 
-        daoImpl.removeInstanceEntities(parentTypeId);
-        Assert.assertTrue(daoImpl.loadInstanceEntities(parentTypeId).isEmpty());
+        dao.removeInstanceEntities(parentTypeId);
+        Assert.assertTrue(dao.loadInstanceEntities(parentTypeId).isEmpty());
+    }
+
+    @Test
+    public void test_updateInstanceEntity() {
+        final long parentTypeId = new RandomDataGenerator().nextLong(1L, 1000L);
+
+        final InstanceEntity entity1 = new InstanceEntity();
+        entity1.setParentTypeId(parentTypeId);
+        entity1.setTypeId(1L);
+        entity1.setAccessModifier(AccessModifier.PUBLIC.name());
+        entity1.setName("entity1");
+        dao.saveInstanceEntity(entity1);
+
+        entity1.setAccessModifier(AccessModifier.PRIVATE.name());
+        final InstanceEntity updated = dao.updateInstanceEntity(entity1);
+
+        Assert.assertThat(updated.getParentTypeId(), is(parentTypeId));
+        Assert.assertThat(updated.getTypeId(), is(1L));
+        Assert.assertThat(updated.getName(), is("entity1"));
+        Assert.assertThat(updated.getAccessModifier(), is("PRIVATE"));
     }
 
     @Test
@@ -142,14 +161,14 @@ public class TypeDaoImplTest {
         entity1.setTypeId(1L);
         entity1.setAccessModifier(AccessModifier.PUBLIC.name());
         entity1.setName("entity1");
-        daoImpl.saveInstanceEntity(entity1);
+        dao.saveInstanceEntity(entity1);
 
-        final List<InstanceEntity> loaded = daoImpl.loadInstanceEntities(entity1.getParentTypeId());
+        final List<InstanceEntity> loaded = dao.loadInstanceEntities(entity1.getParentTypeId());
         Assert.assertThat(loaded.size(), is(1));
         Assert.assertThat(loaded.iterator().next().getName(), is("entity1"));
 
-        daoImpl.removeInstanceEntity(entity1);
-        Assert.assertTrue(daoImpl.loadInstanceEntities(entity1.getParentTypeId()).isEmpty());
+        dao.removeInstanceEntity(entity1);
+        Assert.assertTrue(dao.loadInstanceEntities(entity1.getParentTypeId()).isEmpty());
     }
 
     @Test
@@ -160,22 +179,22 @@ public class TypeDaoImplTest {
         entity1.setParentTypeId(parentTypeId);
         entity1.setTypeId(1L);
         entity1.setCategory(RelationCategory.ASSOCIATION.name());
-        daoImpl.saveRelationEntity(entity1);
+        dao.saveRelationEntity(entity1);
 
         final RelationEntity entity2 = new RelationEntity();
         entity2.setParentTypeId(parentTypeId);
         entity2.setTypeId(2L);
         entity2.setCategory(RelationCategory.ASSOCIATION.name());
-        daoImpl.saveRelationEntity(entity2);
+        dao.saveRelationEntity(entity2);
 
-        final List<RelationEntity> loadedEntities = daoImpl.loadRelationEntities(parentTypeId);
+        final List<RelationEntity> loadedEntities = dao.loadRelationEntities(parentTypeId);
         Assert.assertThat(loadedEntities.size(), is(2));
         final Set<Long> typeIds = loadedEntities.stream().map(RelationEntity::getTypeId).collect(Collectors.toSet());
         Assert.assertTrue(typeIds.contains(1L));
         Assert.assertTrue(typeIds.contains(2L));
 
-        daoImpl.removeRelationEntities(parentTypeId);
-        Assert.assertTrue(daoImpl.loadRelationEntities(parentTypeId).isEmpty());
+        dao.removeRelationEntities(parentTypeId);
+        Assert.assertTrue(dao.loadRelationEntities(parentTypeId).isEmpty());
     }
 
     @Test
@@ -184,15 +203,15 @@ public class TypeDaoImplTest {
         entity.setParentTypeId(new RandomDataGenerator().nextLong(1L, 1000L));
         entity.setTypeId(new RandomDataGenerator().nextLong(1L, 1000L));
         entity.setCategory(TypeCategory.CLASS.name());
-        daoImpl.saveRelationEntity(entity);
+        dao.saveRelationEntity(entity);
 
-        final List<RelationEntity> loaded = daoImpl.loadRelationEntities(entity.getParentTypeId());
+        final List<RelationEntity> loaded = dao.loadRelationEntities(entity.getParentTypeId());
         Assert.assertThat(loaded.size(), is(1));
         Assert.assertThat(loaded.get(0), is(entity));
 
         entity.setCategory("updated");
-        daoImpl.updateRelationEntity(entity);
-        final List<RelationEntity> updated = daoImpl.loadRelationEntities(entity.getParentTypeId());
+        dao.updateRelationEntity(entity);
+        final List<RelationEntity> updated = dao.loadRelationEntities(entity.getParentTypeId());
         Assert.assertThat(updated.size(), is(1));
         Assert.assertThat(updated.get(0), is(entity));
     }
@@ -203,13 +222,13 @@ public class TypeDaoImplTest {
         entity1.setParentTypeId(new RandomDataGenerator().nextLong(1L, 1000L));
         entity1.setTypeId(1L);
         entity1.setCategory(RelationCategory.ASSOCIATION.name());
-        daoImpl.saveRelationEntity(entity1);
+        dao.saveRelationEntity(entity1);
 
-        final List<RelationEntity> loaded = daoImpl.loadRelationEntities(entity1.getParentTypeId());
+        final List<RelationEntity> loaded = dao.loadRelationEntities(entity1.getParentTypeId());
         Assert.assertThat(loaded.size(), is(1));
         Assert.assertThat(loaded.iterator().next().getCategory(), is(RelationCategory.ASSOCIATION.name()));
 
-        daoImpl.removeRelationEntity(entity1);
-        Assert.assertTrue(daoImpl.loadRelationEntities(entity1.getParentTypeId()).isEmpty());
+        dao.removeRelationEntity(entity1);
+        Assert.assertTrue(dao.loadRelationEntities(entity1.getParentTypeId()).isEmpty());
     }
 }
